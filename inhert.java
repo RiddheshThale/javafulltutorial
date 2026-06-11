d62574726b0ce1f34d4e216b68b47f2eccbaaea4
Riddhesh Nivas Thale <riddheshthale@gmail.com>
2026-06-11 16:09:36 +0530

    day2

---
+++ inhert.java
@@ -0,0 +1,18 @@
+class Calc {
+  public int add(int n1, int n2) {
+    return n1 + n2;
+  }
+
+  public int sub(int n1, int n2) {
+    return n1 - n2;
+  }
+}
+
+public class inhert {
+  public static void main(String[] args) {
+    Calc obj = new Calc();
+    int r1 = obj.add(4, 5);
+    int r2 = obj.sub(4, 3);
+    System.out.println(r1 + "\n " + r2);
+  }
+}
---
+++ name.java
@@ -0,0 +1,9 @@
+public class name {
+  public static void main(String[] args) {
+
+  }
+}
+// camel casting
+// class is always starts with capital letter
+// variables and methods always start with small letter
+// constant always have all capital letters
