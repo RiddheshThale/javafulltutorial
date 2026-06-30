8641701ce25c2ed2c831d1b8ae3b90ff8c94c837
Riddhesh Nivas Thale <riddheshthale@gmail.com>
2026-06-30 19:16:49 +0530

    5

---
+++ collect.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class collect {
+  public static void main(String[] args) {
+    Collection<Integer> nums = new ArrayList<Integer>();
+    nums.add(6);
+    nums.add(5);
+    nums.add(8);
+    nums.add(2);
+
+    System.out.println(nums);
+  }
+}
